0c5fe50e8e659ac1bff0090c5abee983f947945a
Fahad Nisar <fnisar142@gamil.com>
2022-07-31 10:47:23 +0500

    Given Layout a name

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import SocialMedia from "../SocialMedia/SocialMedia";
 
 import Styles from "./layout.module.css";
 
-export default function () {
+export default function Layout () {
   return (
     <div className={Styles.aside}>
       <div className={Styles.layout}>
